eac2285dde9de41a1bf245e5ae892f0af40eb1d3
Mikey Badr <mfbadr@gmail.com>
2014-10-14 22:07:07 -0500

    calculations
    Added calculations and correct authorship

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -4,20 +4,79 @@
   angular.module('mb-calc')
   .controller('MainCtrl', ['$scope', function($scope){
     $scope.title = 'mb-calc';
+    $scope.temp = 0;
     $scope.display = '0';
 
     $scope.clear = function(){
       $scope.display = '0';
 
     };
+
+    $scope.decimal = function(){
+      if($scope.display.indexOf('.') === -1){
+        $scope.display += '.';
+      }
+    };
+
     $scope.number = function(num){
-      if($scope.display === '0' && num !== '.'){
-        //just make a different decimal function
-        //should check if there is already a decimal in display
+      if($scope.display === '0'){
         $scope.display = num.toString();
       }else{
         $scope.display += num;
       }
+    };
+
+    $scope.operator = function(op){
+      $scope.temp = parseFloat($scope.display);
+
+
+      switch (op){
+        case '+':
+          $scope.currentOp = '+';
+          $scope.tempDisplay = $scope.display + '+';
+          $scope.display = '0';
+          break;
+        case '-':
+          $scope.currentOp = '-';
+          $scope.tempDisplay = $scope.display + '-';
+          $scope.display = '0';
+          break;
+        case '/':
+          $scope.currentOp = '/';
+          $scope.tempDisplay = $scope.display + '/';
+          $scope.display = '0';
+          break;
+        case '*':
+          $scope.currentOp = '*';
+          $scope.tempDisplay = $scope.display + '*';
+          $scope.display = '0';
+          break;
+      }
+
+      $scope.calculate = function(){
+        switch($scope.currentOp){
+          case '+':
+            $scope.result = $scope.temp + parseFloat($scope.display);
+            $scope.display = $scope.result.toString();
+            break;
+          case '-':
+            $scope.result = $scope.temp - parseFloat($scope.display);
+            $scope.display = $scope.result.toString();
+            break;
+          case '*':
+            $scope.result = $scope.temp * parseFloat($scope.display);
+            $scope.display = $scope.result.toString();
+            break;
+          case '/':
+            $scope.result = $scope.temp / parseFloat($scope.display);
+            $scope.display = $scope.result.toString();
+            break;
+        }
+        $scope.currentOp = null;
+        $scope.tempDisplay = '';
+
+
+      };
 
     };
   }]);
